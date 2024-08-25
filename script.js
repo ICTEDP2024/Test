38991f0cf91136e3bc9bfcc93f1df82265b068e9
ICTEDP2024 <ictdcedp2023@gmail.com>
2024-08-25 09:46:21 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.getElementById('clickMeButton').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'Button was clicked!';
-});
+function handleButtonClick(event) {
+    const clickedButton = event.target;
+    const group = clickedButton.dataset.group;
+
+    const groupButtons = document.querySelectorAll(`.btn[data-group="${group}"]`);
+
+    groupButtons.forEach(button => button.classList.remove('checked'));
+
+    clickedButton.classList.add('checked');
+}
+
+document.querySelectorAll('.btn').forEach(button => button.addEventListener('click', handleButtonClick));
